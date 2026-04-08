bcea06d149313647acf39d314dc0302bb4576014
IvshinKonstantin <ivshin1997@gmail.com>
2026-04-08 09:46:16 +0500

    Add files via upload

---
+++ IName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rewiew_new_4_4
+{
+    internal class IName
+    {
+    }
+}
